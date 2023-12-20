9153d2fe46a6c64646bda2670eac214a57a3432e
filizhelvaci <filizhelvaci82@gmail.com>
2023-12-20 09:42:53 +0300

    SayisalVeriTipleri.java

---
+++ SayisalVeriTipleri.java
@@ -0,0 +1,12 @@
+
+public class SayisalVeriTipleri {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        byte abc=120;
+        System.out.println("Byte değeri: " + abc);
+        String s= "Nasılsın kardeş?";
+        System.out.println(s);
+	}
+
+}
